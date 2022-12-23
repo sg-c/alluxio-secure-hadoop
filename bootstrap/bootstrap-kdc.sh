467f2cb2197828499373908a33906d73564d04f4
Saiguang Che <saiguang@alluxio.com>
2022-12-23 14:29:22 +0800

    try sth

--- bootstrap/bootstrap-kdc.sh
+++ bootstrap/bootstrap-kdc.sh
@@ -2,22 +2,11 @@
 
 set -e
 
-echo "bootstrap 111"
-
-# execute the default entrypoint first
-/config.sh
-
-echo "bootstrap 222"
-
-# /shared/krb5 is mounted by a named volume 
-# and shared by services in the same compose app
-echo "includedir /etc/krb5.conf.d" > /shared/krb5/krb5.conf
-cat /etc/krb5.conf >> /shared/krb5/krb5.conf
-
 # add realms and domain_realm to an independent krb5.conf for sharing
 : ${KDC_ADDRESS:=$(hostname -f)}
 : ${KERB_ADMIN_PORT:=749}
 : ${KERB_KDC_PORT:=88}
+
 cat>/etc/krb5.conf.d/krb5.${REALM}.conf<<EOF
 [realms]
    $REALM = {
@@ -29,4 +18,5 @@ cat>/etc/krb5.conf.d/krb5.${REALM}.conf<<EOF
    $DOMAIN_REALM = $REALM
 EOF
 
-echo "bootstrap 333"
+# execute the default entrypoint first
+/config.sh
