45f9ec8b4ca7c164341c5719879c397b8712e6d2
Saiguang Che <saiguang@alluxio.com>
2022-12-23 11:26:50 +0800

    try sth

--- bootstrap/bootstrap-kdc.sh
+++ bootstrap/bootstrap-kdc.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# /tmp/etc/krb5.conf is mounted by a named volume 
+# /shared/krb5 is mounted by a named volume 
 # and shared by services in the same compose app
-echo "includedir /etc/krb5.conf.d" > /tmp/etc/krb5.conf
-cat /etc/krb5.conf >> /tmp/etc/krb5.conf
+echo "includedir /etc/krb5.conf.d" > /shared/krb5/krb5.conf
+cat /etc/krb5.conf >> /shared/krb5/krb5.conf
 
 # add realms and domain_realm to an independent krb5.conf for sharing
-cat>/etc/krb5.conf.d/krb5.${REALM}.conf<EOF
+cat>/etc/krb5.conf.d/krb5.${REALM}.conf<<EOF
 [realms]
    $REALM = {
       kdc = $KDC_ADDRESS:$KERB_KDC_PORT
